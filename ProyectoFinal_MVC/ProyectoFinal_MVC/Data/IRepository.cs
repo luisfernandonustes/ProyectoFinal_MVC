02a44f4dd2bdba948fc0df055602bb972f36e2e9
luisfernandonustes <lfnustes@misena.edu.co>
2022-09-06 17:40:39 -0500

    commit Date

---
+++ ProyectoFinal_MVC/ProyectoFinal_MVC/Data/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using ProyectoFinal_MVC.Models;
+
+namespace ProyectoFinal_MVC.Data
+{
+    public interface IRepository
+    {
+        public Task Save<T>(T obj) where T : Entity;
+        public void Delete<T>(T obj) where T : Entity;
+        public void Update<T>(T obj) where T : Entity;
+        public Task<List<T>> GetAll<T>() where T : Entity;
+        public Task Commit();
+    }
+}
---
+++ ProyectoFinal_MVC/ProyectoFinal_MVC/Data/ProyectoFinal_MVCDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectoFinal_MVC.Models;
+
+namespace ProyectoFinal_MVC.Data
+{
+    public class ProyectoFinal_MVCDbContext : DbContext
+    {
+        public DbSet<EmpresaConstructora> EmpresaConstructoras { get; set; }
+        public DbSet<Trabajador> Trabajadores { get; set; }
+        public ProyectoFinal_MVCDbContext(DbContextOptions<ProyectoFinal_MVCDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
